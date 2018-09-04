6e0942336eddc4e400299f241c137130464c5135
Sakis Kotisis <s.kotisis@rheagroup.com>
2018-09-04 17:27:02 +0200

    Fixed telegraf playbook call.
    Change-Id: I81a514b0ff48f3cb2d7672edcc2764899ef69fd2

--- exoscale/ceph.tf
+++ exoscale/ceph.tf
@@ -88,7 +88,7 @@ resource "null_resource" "deploy-ceph" {
   provisioner "remote-exec" {
     inline = [
       "ansible-playbook playbooks/myceph/myceph.yml -i inventory-ceph.ini --extra-vars \"osd_disks=${var.disks-per-osd_count} vol_prefix=${var.vol_prefix}\" -f 50 -T 30",
-	  "ansible-playbook playbooks/roles/telegraf/test.yml -i inventory-ceph.ini --extra-vars '{\"telegraf_influxdb_urls\":[\"http://\" + ${exoscale_compute.grafana.ip_address} + \":8086\"]}' ",
+	  "ansible-playbook playbooks/roles/telegraf/test.yml -i inventory-ceph.ini --extra-vars \"{'telegraf_influxdb_urls':['http://' + ${exoscale_compute.grafana.ip_address} + ':8086']}\" ",
     ]
   }
   provisioner "local-exec" {
